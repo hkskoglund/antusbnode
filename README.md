22d945e2359c2e183a9bd6c5e6f8133dff4ea194
Henning Knut Skoglund <hkskoglund@gmail.com>
2015-04-10 10:58:08 +0200

    Updated new version

--- README.md
+++ README.md
@@ -1,73 +1,98 @@
-PRE-REQUISITE:
+#getfit
+A utility to get FIT files from antfs devices. By default new files are downloaded automatically. Files are downloaded to current directory.
 
-  - node.js http://nodejs.org/, tested on v 0.10.xx
-  - node-gyp package for building native modules https://npmjs.org/package/node-gyp
-  - ANT USB2 Wireless Stick nRF24AP2-USB-ANT http://www.nordicsemi.com/eng/Products/ANT/nRF24AP2-USB
+##install
 
-Windows-development:
+```
+npm install getfit
 
-  - LIBUSB-drivers, i.e Windows platform : Zadig http://sourceforge.net/projects/libwdi/files/zadig/
-  - Visual Studio Desktop Express 2012 C++ compiler/IDE
-  
-DEPENDENT-LIBRARIES
+```
 
-  - Interface for LIBUSB - <b>usb</b> https://github.com/nonolith/node-usb
-  - Websocket - <b>ws</b> https://github.com/einaros/ws
-  
-Tested on Windows 8 <b>X64</b>, may work on Linux/Mac/win32 with recompilation of native libraries
+##download
 
-BUILDING/INSTALLING NATIVE LIBRARIES WITH VISUAL STUDIO DESKTOP 2012 EXPRESS
+Download file at index 10
+```
+node getfit -d 10
 
-Based on info. from http://stackoverflow.com/questions/14278417/cannot-install-node-modules-that-require-compilation-on-windows-7-x64-vs2012
+```
+Download file at index 10,11,12
+```
+node getfit -d 10,11,12
 
-npm install usb --msvs_version=2012e
-or
-node-gyp rebuild --msvs_version=2012e
+```
 
-POWERSHELL:
 
-Instead of using the msvs_version option for node-gyp, the GYP_MSVS_VERSION environment variable can be set to 2012e/2012 for the 
-user/machine-profile to make this permanent.
+##ls
+List directory on device
+```
+node getfit -l
+Host getfit connecting to device 0 on channel 0
+totals 4307
+--w-rw--- 1 getfit antfs     1900544 Dec 31  1989 Manufacturer-1
+----rw--- 1 getfit antfs      262144 Dec 31  1989 Manufacturer-2
+-r--rw--- 1 getfit antfs         574 Dec 31  1989 DeviceCapabilities-3.fit
+-rw-rw--- 1 getfit antfs         680 Dec 31  1989 Settings-4.fit
+-rw-rw--- 1 getfit antfs        1159 Dec 31  1989 SportSettings-5.fit
+-rw-rw--- 1 getfit antfs         317 Dec 31  1989 SportSettings-6.fit
+-rw-rw--- 1 getfit antfs        1159 Dec 31  1989 SportSettings-7.fit
+-rw-rw--- 1 getfit antfs         317 Dec 31  1989 SportSettings-8.fit
+-rw-rw--- 1 getfit antfs        1197 Dec 31  1989 SportSettings-9.fit
+-r--rw--- 1 getfit antfs       36056 Apr 09 17:57 Activity-10.fit
+-rw-rw--- 1 getfit antfs          72 Dec 31  1989 Schedule-11.fit
+-rw-rw--- 1 getfit antfs         627 Dec 31  1989 Locations-12.fit
+-r--rw--- 1 getfit antfs          72 Dec 31  1989 Weight-13.fit
+-r--rw--- 1 getfit antfs         168 Dec 31  1989 Totals-14.fit
+-rw-rw--- 1 getfit antfs          72 Dec 31  1989 Goals-15.fit
 
-  [Environment]::SetEnvironmentVariable("GYP_MSVS_VERSION","2012e","User")
-  
-Restart powershell to activate. 
 
-The detailed algorithm for detecting visual studio versions is available at; https://github.com/TooTallNate/node-gyp/blob/master/gyp/pylib/gyp/MSVSVersion.py#L200-L265
+```
 
-USAGE:
+##erase
+Erase file at index 10
+```
+node getfit -e 10
 
-<b>node getfit</b> 
+```
 
-  listen for LINK broadcasts from an ANT-FS enabled device i.e FR910 XT/610 and downloads new files
-  
-<b>node getfit -d 10</b>
+##usbport
 
-  downloads file at index 10
-  
-<b>node getfit -d</b> 
+By default getfit will use the first available ant device it found during scanning of the usb ports, i.e port 0 is used unless. All channels are reset.
 
-  list device directory
-  
-<b>node getfit -b</b>
+Use usb port 1
+```
+node getfit -p 1
 
-  uses background scanning channel for ANT+ and establishes a channel when a new device/master is found. Data is written to websocket
-  on i.e localhost:8093 (hostname/port can be change by editing configuration.json)
-  
-<b>node getfit -c</b>
+```
 
-  uses a continous scanning channel for ANT+ and writes data to websocket on i.e localhost:8093
-  
-<b>node getfit -e 10</b>
+##device number
+By default any device is searched for, i.e 0 is used for device number.
+Search for device 1
+```
+node getfit -n 1
+```
 
-  erase file at index 10
-  
-Support for visualizing data from sensors are available using "Live stream" in FITView https://github.com/hkskoglund/FITView or http://fitview.azurewebsites.net.
-Currently testing has been done on device profile for HRM, SDM4 and SPDCAD (GSC-10). SDM4 sends background pages with
-sensor health and battery status. A low battery is visualized with an icon and SDM health statues other than OK will be
-reported. If FIT settings are imported into FITView, <i>calibration</i> factors for bike wheelsize or footpod is taken into account
-when calculating speed and distance.
 
-<b>configuration.json</b>
+##list ant devices
+```
+node getfit -u
+0 Bus 1 Number 7: ID fcf:1008
+1 Bus 4 Number 2: ID fcf:1009
+```
 
-JSON file for configuration of ANT+/ANT-FS network keys, websocket server hostname and port, and usb vendor/product id.
+##logging
+
+Show detailed logging of protocol negotiation as well as usb traffic
+```
+node getfit -v
+
+```
+
+##libusb debug
+Specify libusb log level 0 - 4
+```
+node getfit -L {loglevel}
+
+```
+
+##licence
+MIT
--- getfit.js
+++ getfit.js
@@ -1,379 +1,79 @@
-#! /usr/bin/env node
-"use strict";
-//console.log("require.main",require.main);
+var channel = 0,
+  net = 0,
 
-// Some info. from a python implementation of ANTFS: https://github.com/Tigge/Garmin-Forerunner-610-Extractor
+  devices,
+  hostname = 'getfit',
+  argv = require('minimist')(process.argv.slice(2)),
+  antHost = new(require('libantjs'))({
+    log: argv.v,
+    debugLevel: argv.L || 0 // 0 - 4 enables libusb debug info.
+  }),
+  antfsHost,
+  deviceNumber = argv.n || 0, // 0 for any device
+  usbPort = argv.p || 0;
 
-var util = require('util'),
-    events = require('events'),
-    Channel = require('./node_modules/ant-lib/channel.js'),
-    ANT = require('ant-lib'),
-   // Channel = require('./node_modules/ant-lib/channel.js'),
-    DeviceProfile_ANTFS = require('./node_modules/ant-lib/profiles/deviceProfile_ANTFS.js'),
-    BackgroundScanningChannel = require('./node_modules/ant-lib/profiles/backgroundScanningChannel.js'),
-    ContinousScanningChannel = require('./node_modules/ant-lib/profiles/ContinousScanningChannel.js'),
-    fs = require('fs'),
-    getFitProgram = require('commander'),
-    // Based on http://nodejs.org/api/modules.html#modules_accessing_the_main_module
-    runDirectly = (require.main === module) ? true : false,
-    PathSeparator = require('path').sep;
+function onError(error) {
+  console.trace();
+  console.error('error', error);
+}
 
-function GetFIT() {
-
-    var self = this;
-
-    self._configuration = {
-    };
-           
-    function list(val) {
-        return val.split(',');
-    }
-
-    // require.main === module if run directly via "node getfit"
-    if (runDirectly) {
-
-        self.noopWaitID = setInterval(function _noop() {}, 60*1000*60*24);
-
-        process.on('SIGINT', function _sigint() {
-            clearInterval(self.noopWaitID);
-        });
-
-        console.log("GetFIT version " + GetFIT.prototype.VERSION, "PID",process.pid,"node version", process.versions.node, "V8", process.versions.v8, "on " + process.platform + " " + process.arch);
-
-        GetFIT.prototype.STARTUP_DIRECTORY = process.argv[1].slice(0, process.argv[1].lastIndexOf(PathSeparator));
-        //console.log("process.argv", process.argv);
-        console.log(Date.now() + " Process startup directory :", GetFIT.prototype.STARTUP_DIRECTORY);
-        
-        self._configuration = self.getConfigurationFromFile(GetFIT.prototype.STARTUP_DIRECTORY + PathSeparator+'configuration.json');
-
-        if (typeof self._configuration === "undefined")
-            process.exit(GetFIT.prototype.ERROR.CONFIGURATION_NOT_FOUND);
-
-        getFitProgram.version(GetFIT.prototype.VERSION)
-           // .option('-n,--new', 'Download FIT files flagged as new')
-            .option('-l,--list', 'List directory of ANT-FS device')
-            .option('-d,--download <items>', "Download FIT files at index '<n1>,<n2>,...'", list)
-            .option('-a,--download-all', 'Download the entire index of FIT files')
-            .option('-e,--erase <items>', "Erase FIT file at index '<n1>'", list)
-            .option('-b,--background', 'Background search channel for ANT+ devices + websocket server sending ANT+ pages for HRM/SDM4/SPDCAD device profile')
-            .option('-c,--continous', 'Continous scan mode for ANT+ devices + websocket server sending ANT+ pages for HRM/SDM4/SPDCAD device profile')
-            .parse(process.argv);
-
-        //if (getFitProgram.new)
-        //    self._commandQueue.push(DeviceProfile_ANTFS.prototype.NODECOMMAND.DOWNLOAD_NEW);
-
-        if (getFitProgram.list) 
-            this.executeANTFSCommand(GetFIT.prototype.ANTFS_COMMAND.DOWNLOAD_MULTIPLE,undefined);
-        else
-            if (getFitProgram.download) 
-                this.executeANTFSCommand(GetFIT.prototype.ANTFS_COMMAND.DOWNLOAD_MULTIPLE,getFitProgram.download);
-            else
-                if (getFitProgram.downloadAll) 
-                    this.executeANTFSCommand(GetFIT.prototype.ANTFS_COMMAND.DOWNLOAD_ALL,undefined);
-                else
-                    if (getFitProgram.erase) 
-                        this.executeANTFSCommand(GetFIT.prototype.ANTFS_COMMAND.ERASE_MULTIPLE,getFitProgram.erase);
-                    else
-                        if (getFitProgram.background) {
-                            this._backgroundScanningChannel = new BackgroundScanningChannel(this.getConfiguration());
-                            this._backgroundScanningChannel.initANT(function _cb () {
-                                console.log(Date.now(), "Enabling channel for background scanning for ANT+ devices");
-                                this.enableChannelConfiguration(this.getSlaveChannelConfiguration({
-                                    "networkNr": 0,
-                                    "channelNr": 0,
-                                    "deviceNr": Channel.prototype.CHANNELID.DEVICE_NUMBER_WILDCARD,
-                                    "deviceType": Channel.prototype.CHANNELID.DEVICE_TYPE_WILDCARD,
-                                    "transmissionType": Channel.prototype.CHANNELID.TRANSMISSION_TYPE_WILDCARD,
-                                    "searchTimeoutHP": ANT.prototype.SEARCH_TIMEOUT.DISABLE_HIGH_PRIORITY_SEARCH_MODE,
-                                    "searchTimeoutLP" :  ANT.prototype.SEARCH_TIMEOUT.INFINITE_SEARCH
-                                }));
-                            }.bind(this._backgroundScanningChannel));
-                        }
-                        else
-                            if (getFitProgram.continous) {
-                                console.log(Date.now(), "Using continous scan mode for ANT+ devices");
-                                this.setScanningChannel("continous");
-                            }
-                            else // Default to enabling ANT_FS device profile 
-                                this.executeANTFSCommand(undefined,undefined);
-    }
+function onConnect(error) {
+  if (error) {
+    console.log('onConnect', error);
+    return;
+  }
 
 }
 
-GetFIT.prototype = {
+function onANTexited(err) {
+  //  if (!err)
+  //    console.log(Date.now() + ' Exit ANT');
+}
 
-    ANTFS_COMMAND : {
-        "DOWNLOAD_MULTIPLE" : DeviceProfile_ANTFS.prototype.NODECOMMAND.DOWNLOAD_MULTIPLE,
-        "DOWNLOAD_ALL" : DeviceProfile_ANTFS.prototype.NODECOMMAND.DOWNLOAD_ALL,
-        "ERASE_MULTIPLE" : DeviceProfile_ANTFS.prototype.NODECOMMAND.ERASE_MULTIPLE
-    },
+function onANTInited(error, notificationStartup) {
 
-        ERROR : {
-            "CONFIGURATION_NOT_FOUND" : 0x01
-        },
+  if (error) {
+    console.log('onantinited', error);
+    return;
+  }
 
-    VERSION: "0.1.0",
+  antHost.on('transport_end', function() {
+    antHost.exit(onANTexited);
+  });
 
-    DEFAULT_WEBSOCKET_HOST: 'localhost',
-    DEFAULT_WEBSOCKET_PORT: 8093,
+  antHost.on('open', function(ch) {
+    console.log('Host ' + hostname + ' connecting to device ' + deviceNumber + ' on channel ' + ch);
+  });
 
-    executeANTFSCommand : function (command,index)
-    {
-        
-        this.ANTFS_profile = new DeviceProfile_ANTFS(this.getConfiguration());
-        var self = this.ANTFS_profile;
-        if (typeof command !== "undefined")
-            this.ANTFS_profile.addCommand(command);
-        if (typeof index !== "undefined") {
-            this.ANTFS_profile.addIndex(index);
-        }
+  antHost.connectANTFS(channel, net, deviceNumber, hostname, argv.d, argv.e, argv.l, onConnect);
 
-        //try {
-        this.ANTFS_profile.initANT(function _initANTCB (error) {
-            
-            if (error){
-                //console.trace();
-                console.log("Exit.",error);
-                process.exit();
-            }
-                console.log(Date.now(), "Enabling channel for ANT_FS client");
-                self.enableChannelConfiguration(self.getSlaveChannelConfiguration({
-                    "networkNr": 0,
-                    "channelNr": 0,
-                    // Timeout : Math.round(60 / 2.5)
-                    "deviceNr": Channel.prototype.CHANNELID.DEVICE_NUMBER_WILDCARD,
-                    "deviceType": Channel.prototype.CHANNELID.DEVICE_TYPE_WILDCARD,
-                    "transmissionType": Channel.prototype.CHANNELID.TRANSMISSION_TYPE_WILDCARD,
-                    "searchTimeoutHP": ANT.prototype.SEARCH_TIMEOUT.INFINITE_SEARCH
-                }));
-            });
-        //} catch (err)
-        //{
-        //    console.log("ERROR",err);
-        //    console.trace();
-        //}
-    },
+}
 
-    getConfiguration : function ()
-    {
-        return this._configuration;
-    },
+devices = antHost.getDevices();
 
-    
+if (!devices || !devices.length)
+  console.error('No ANT devices available');
 
-    verifyConfiguration : function ()
-    {
-        if (typeof this._configuration === "undefined")
-            this._configuration = {};
+else if (argv.u) {
+  console.log(antHost.listDevices());
+} else {
 
-    },
+  try {
 
-    setNetworkKey : function (name,key)
-    {
-        this.verifyConfiguration();
-        if (typeof this._configuration.network_keys === "undefined")
-            this._configuration.network_keys = {};
+      antHost.init(usbPort, onANTInited);
 
-        this._configuration.network_keys[name] = key;
-    },
-
-    setUSBDevice : function (name,idVendor,idProduct)
-    {
-        this.verifyConfiguration();
-        if (typeof this._configuration.usb === "undefined")
-            this._configuration.usb = {};
-
-        this._configuration.usb.name = name;
-        this._configuration.usb.idVendor = idVendor;
-        this._configuration.usb.idProduct = idProduct;
-    },
-
-    setWebsocketServer : function (host,port)
-    {
-        this.verifyConfiguration();
-        if (typeof this._configuration.websocket === "undefined")
-            this._configuration.websocket = {};
-
-        this._configuration.websocket.host = host;
-        this._configuration.websocket.port = port;
-    },
+  } catch (err) {
+    onError(err);
+  }
+}
 
 
-    setConfiguration : function (configuration)
-    {
-        this._configuration = configuration;
-    },
-
-    getConfigurationFromFile : function (configurationFileName)
-    {
-        if (fs.existsSync(configurationFileName)) {
-            return JSON.parse(fs.readFileSync(configurationFileName));
-        } else {
-            console.log("Did not find configuration filename ", configurationFileName);
-            return undefined;
-        }
-    },
-
-   
-
-    getWebSocketServer : function ()
-    {
-        return self.wss;
-    },
-    
-    // Broadcast data to all clients
-    broadCastOnWebSocket:  function (data) {
-         var self = this;
-
-         //console.log(self.wss);
-
-         if (typeof self.wss === "undefined") {
-             console.warn(Date.now()+" WEBSOCKET: no websocket server available on "+self._configuration.websocket.host+":"+self._configuration.websocket.port," broadcast failed ", data);
-             return;
-         }
-
-         var len = self.wss.clients.length;
-         //console.log("Length of clients", len);
-         for (var clientNr = 0; clientNr < len; clientNr++) {
-             if (typeof self.wss.clients !== "undefined" && self.wss.clients[clientNr] !== "undefined") // Just to make sure we have clientTracking and client is present
-             {
-                 //console.log("Sending data to client nr. ", clientNr, "data:",data);
-                 //console.log(self.wss.clients[clientNr]);
-                 self.wss.clients[clientNr].send(data);
-             } else
-                 console.warn("Found no clients to send data to, is websocket server operative?");
-         }
-     },
-
-    //stop: function () {
-    //    var self = this;
-
-    //    clearInterval(self.heartBeatIntervalID);
-
-    //    if (typeof self.wss !== "undefined") {
-    //        console.log(Date.now()+ " Closing websocket server");
-    //        self.wss.close();
-    //    }
-
-    //    if (typeof self._configuration.deviceProfile !== "undefined") {
-    //        self._configuration.deviceProfile.stop();
-    //    } else
-    //        console.warn(Date.now(),"Found no device profile attached to configuration, unable to stop USB ANT device gracefully"); 
-    //},
-
-   
-
-    //start: function () {
-    //    // console.log(process);
-    //    //var self = this,
-    //    //    scanningChannel,
-    //    //    heartBeat = 0,
-    //    //    beatFunc = function ()
-    //    //    {
-    //    //        heartBeat++;
-    //    //    };
-
-    //    ////console.log("Configuration object", self._configuration);
-
-    //    ////self.heartBeat = 0;
-    //    //self.heartBeatIntervalID = setInterval(beatFunc, 60000 * 60 * 24); // 1 "beat" each day 
-
-    //    //// Handle gracefull termination
-    //    //// http://thomashunter.name/blog/gracefully-kill-node-js-app-from-ctrl-c/
-
-    //    //process.on('SIGINT', function sigint() {
-    //    //    console.log(Date.now() + " Process interrupted - signal SIGINT (Ctrl+C)");
-
-    //    //    // TO DO:  self.deviceProfile_ANTFS.sendDisconnect.call(self); // Disconnect
-
-    //    //    self.stop();
-
-    //    //});
-
-    //    // Lesson : ANT-FS and HRM on different network due to different keys
-    //    // Seems like : Cannot simultaneously listen to broadcasts from ANT-FS =  2450 MHz and HRM/Bike spd/Stride sensor = 2457 Mhz, but with different msg. periode
-
-       
-
-    //    if (self._configuration.scanningChannel === "continous") {
-    //        console.log(Date.now(), "Setting channel configuration for continous channel");
-    //        scanningChannel = new ContinousScanningChannel(this);
-          
-    //        self.ANT.setChannelConfiguration(0, scanningChannel.getSlaveChannelConfiguration(0, 0,
-    //            Channel.prototype.CHANNELID.DEVICE_NUMBER_WILDCARD, Channel.prototype.CHANNELID.DEVICE_TYPE_WILDCARD, Channel.prototype.CHANNELID.TRANSMISSION_TYPE_WILDCARD,
-    //            ANT.prototype.SEARCH_TIMEOUT.INFINITE_SEARCH, ANT.prototype.ANT_FREQUENCY, new Buffer(self._configuration.network_keys.ANT_PLUS)));
-    //        self.ANT.activateChannelConfiguration(0, function error(err) { console.log("Could not configure continous scanning channel ANT+ ", err); },
-    //                        function successCB(data) {
-    //                            openChannel();
-    //                        });
-    //        this.startWebSocketServer();
-    //    } else
-    //        if (self._configuration.scanningChannel === "background") {
-    //            console.log(Date.now(), "Setting channel configuration for background channel");
-    //            scanningChannel = new BackgroundScanningChannel(this);
-    //            self.ANT.setChannelConfiguration(0, scanningChannel.getSlaveChannelConfiguration(0, 0,
-    //                Channel.prototype.CHANNELID.DEVICE_NUMBER_WILDCARD, 
-    //              Channel.prototype.CHANNELID.DEVICE_TYPE_WILDCARD, 
-    //              Channel.prototype.CHANNELID.TRANSMISSION_TYPE_WILDCARD,
-    //                ANT.prototype.SEARCH_TIMEOUT.INFINITE_SEARCH, ANT.prototype.ANT_FREQUENCY, new Buffer(self._configuration.network_keys.ANT_PLUS)));
-    //            console.log(self.ANT.channelConfiguration);
-    //            self.ANT.activateChannelConfiguration(0, function error(err) { console.log("Could not configure background scanning channel ANT+ ", err); },
-    //           function successCB(data) {
-    //               openChannel();
-    //           });
-    //            this.startWebSocketServer();
-    //        } else {
-              
-    //        }
-        
-
-    //},
-
-    startWebSocketServer: function () {
-        var self = this;
-
-        if (typeof self._configuration.websocket === "undefined") {
-            console.warn(Date.now(), "No websocket configuration specified, will not broadcast sensor data on websocket");
-            return;
-        } else if (typeof self._configuration.websocket.host === "undefined") {
-            console.warn(Date.now(), "No websocket host, default host "+GetFIT.prototype.DEFAULT_WEBSOCKET_HOST +" used");
-            self._configuration.websocket.host = GetFIT.prototype.DEFAULT_WEBSOCKET_HOST;
-          
-        } else if (typeof self._configuration.websocket.port === "undefined") {
-            console.warn(Date.now(), "No websocket port specifiedn, default port "+ GetFIT.prototype.DEFAULT_WEBSOCKET_PORT+" used");
-            self._configuration.websocket.port = GetFIT.prototype.DEFAULT_WEBSOCKET_PORT;
-        }
-        // Start websocket server
-
-        var WebSocketServer = require('ws').Server;
-        if (typeof WebSocketServer === "undefined") {
-            console.error(Date.now() + " Failed to load websocket module - ws");
-            return;
-        }
-
-        // Client tracking keeps track of websocket server clients in "clients" property -> removed on 'close'
-        self.wss = new WebSocketServer({ host: self._configuration.websocket.host, port: self._configuration.websocket.port, clientTracking: true });
-
-        self.wss.on('listening', function () {
-            console.log(Date.now() + " Live streaming sensor ANT+ pages - WebSocketServer: listening on " + self._configuration.websocket.host + ":" + self._configuration.websocket.port+" for clients");
-        });
-
-        self.wss.on('connection', function (ws) {
-            console.log(Date.now()," Live streaming sensor ANT+ pages - WebSocketServer: New client connected - will receive broadcast data at "+ws.upgradeReq.headers.origin,ws.upgradeReq.url);
-            // console.log(ws);
-            //self.websockets.push(ws); // Keeps track of all incoming websocket clients
-
-            ws.on('message', function (message) {
-                console.log(Date.now() + ' WebSocketServer received: %s', message);
-                //    ws.send('something');
-            });
-        });
-
-        self.wss.on('error', function (error) {
-            console.log(Date.now() + " WebSocketServer: Error ", error);
-        });
-    }
-
-};
-
-module.exports = new GetFIT();
+// options
+// -n device number to search for
+// -d download,
+// -e erase,
+// -l list directory
+// -u show usb ANT devices
+// -p usb device to use
+// -v verbose logging
+// -L {level} libusb log level 0-4
